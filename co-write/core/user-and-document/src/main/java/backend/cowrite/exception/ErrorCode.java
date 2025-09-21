b7ffa707c91f58650d40315a6d42313ee3e2e9ae
jeongseonwoong <tjsdnd1219@naver.com>
2025-09-21 16:51:22 +0900

    feat: 설정 값들 불러오도록 구현

--- co-write/core/user-and-document/src/main/java/backend/cowrite/exception/ErrorCode.java
+++ co-write/core/user-and-document/src/main/java/backend/cowrite/exception/ErrorCode.java
@@ -8,10 +8,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ErrorCode {
 
-    TEMPORARY_ERROR(500,"C_000","임시 에러코드입니다."),
+    TEMPORARY_ERROR(500, "C_000", "임시 에러코드입니다."),
     USER_NOT_FOUND(400, "C_001", "해당하는 유저 정보가 존재하지 않습니다."),
-    DOCS_NOT_FOUND(400, "C_002", "해당하는 문서 정보가 존재하지 않습니다.");
-
+    DOCS_NOT_FOUND(400, "C_002", "해당하는 문서 정보가 존재하지 않습니다."),
+    UNAUTHORIZED_REDIRECT_URI(400, "C_003", "허용되지 않은 리다이렉트 url입니다.");
 
     private final int state;
     private final String code;
