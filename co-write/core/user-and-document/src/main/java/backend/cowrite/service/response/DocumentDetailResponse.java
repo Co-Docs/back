b7997f6de10a76488a82853cba1435a96a8012e0
jeongseonwoong <tjsdnd1219@naver.com>
2025-09-21 13:55:32 +0900

    feat: 문서관련 응답 dto 구현

---
+++ co-write/core/user-and-document/src/main/java/backend/cowrite/service/response/DocumentDetailResponse.java
@@ -0,0 +1,20 @@
+package backend.cowrite.service.response;
+
+import backend.cowrite.entity.Document;
+
+import java.util.List;
+
+public record DocumentDetailResponse(
+        String title, String content, List<UserDocumentDto> userDocuments
+) {
+
+    private static record UserDocumentDto(String nickname){
+
+    }
+
+    public static DocumentDetailResponse of(Document document){
+        return new DocumentDetailResponse(document.getTitle(),
+                document.getContent(),
+                document.getUserDocuments().stream().map(userDocument -> new UserDocumentDto(userDocument.getUser().getNickname())).toList());
+    }
+}
---
+++ co-write/core/user-and-document/src/main/java/backend/cowrite/service/response/DocumentPreviewResponse.java
@@ -0,0 +1,28 @@
+package backend.cowrite.service.response;
+
+import backend.cowrite.entity.Document;
+
+import java.util.List;
+
+public record DocumentPreviewResponse(
+    List<DocumentDto> documents
+) {
+
+    private static record DocumentDto(String title, List<UserDocumentDto> userDocuments){
+
+
+    }
+
+    private static record UserDocumentDto(String nickname){
+
+    }
+
+    public static DocumentPreviewResponse of(List<Document> documents){
+        return new DocumentPreviewResponse(
+                documents
+                .stream()
+                .map(document ->
+                        new DocumentDto(document.getTitle(), document.getUserDocuments().stream().map(userDocument -> new UserDocumentDto(userDocument.getUser().getNickname())).toList()))
+                .toList());
+    }
+}
