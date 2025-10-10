fdea00e5fc9d342f74c06d9b13cf0a031db1f119
jeongseonwoong <tjsdnd1219@naver.com>
2025-10-10 16:46:29 +0900

    fix: 캐시에 username별로 value가 안들어가던 문제 해결

--- co-write/core/user-and-document/src/main/java/backend/cowrite/service/UserService.java
+++ co-write/core/user-and-document/src/main/java/backend/cowrite/service/UserService.java
@@ -4,7 +4,8 @@ import backend.cowrite.entity.User;
 import backend.cowrite.exception.CustomException;
 import backend.cowrite.exception.ErrorCode;
 import backend.cowrite.repository.UserRepository;
-import kuke.board.common.snowflake.Snowflake;
+import backend.cowrite.service.response.UserCacheDto;
+import backend.cowrite.common.snowflake.Snowflake;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
@@ -38,6 +39,7 @@ public class UserService {
     }
 
     public Long register(String username, String password, String passwordConfirm, String nickname, LocalDateTime birth, String email, String phoneNumber) {
+        User.checkPasswordConfirm(password,passwordConfirm);
         String encodedPassword = bCryptPasswordEncoder.encode(password);
         String encodedConfirmPassword = bCryptPasswordEncoder.encode(passwordConfirm);
         User user = User.registerUser(snowflake.nextId(), username, encodedPassword, encodedConfirmPassword, nickname, birth, email, phoneNumber);
@@ -50,11 +52,15 @@ public class UserService {
     public void logout(String username) {
     }
 
-    @Cacheable(cacheNames = "user", key = "'user:username:' + #username", cacheManager = "userCacheManager")
-    public User findByUsername(String username) {
-        return userRepository.findByUsername(username)
+    @Cacheable(cacheManager = "userCacheManager", cacheNames = "user", key = "'user:username:' + #username"
+    )
+    public UserCacheDto findByUsername(String username) {
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND, "해당 username의 유저가 존재하지 않습니다:" + username));
+
+        return new UserCacheDto(user.getUserId(), user.getUsername(), user.getPassword(), user.getNickname(), user.getRole());
     }
+
 
     public User changePassword(String username, String password, String passwordConfirm) {
         User user = userRepository.findByUsername(username)
