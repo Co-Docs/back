3605a700c6e605aeef807f5eae1eb503c665fbfc
jeongseonwoong <tjsdnd1219@naver.com>
2025-10-10 16:35:19 +0900

    fix: redis에서key와 value를 제대로 serialize해서 가져올 수 있도록 수정

--- co-write/core/user-and-document/src/main/java/backend/cowrite/config/RedisCacheConfig.java
+++ co-write/core/user-and-document/src/main/java/backend/cowrite/config/RedisCacheConfig.java
@@ -1,5 +1,9 @@
 package backend.cowrite.config;
 
+import backend.cowrite.service.response.UserCacheDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -19,23 +24,18 @@ public class RedisCacheConfig {
 
     //캐시 매니저는 여러개 만들어 상황에 따라 사용할 수 있다.
     @Bean
-    public CacheManager userCacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
-                .defaultCacheConfig()
-                .serializeKeysWith( //Redis에 Key를 저장할 때 String으로 직렬화해서 저장
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new StringRedisSerializer()))
-                .serializeValuesWith( //Redis에 Value를 저장할 때 Json으로 직렬화해서 저장
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new Jackson2JsonRedisSerializer<Object>(Object.class)
-                        )
-                )// 데이터의 만료기간(TTL) 설정
-                .entryTtl(Duration.ofMinutes(1L));
+    public CacheManager userCacheManager(RedisConnectionFactory connectionFactory) {
+        var keySer   = new org.springframework.data.redis.serializer.StringRedisSerializer();
+        var valueSer = new org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer<>(UserCacheDto.class);
 
-        return RedisCacheManager
-                .RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory)
-                .cacheDefaults(redisCacheConfiguration)
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSer))
+                .entryTtl(Duration.ofMinutes(1));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
                 .build();
     }
+
 }
