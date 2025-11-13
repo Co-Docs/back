04ebf5dd81bdd77c3838792fe5699999f1ecbf1b
jeongseonwoong <tjsdnd1219@naver.com>
2025-11-13 21:02:47 +0900

    fix(EventHandler): 문서 수정 결과로 수정된 내용과 버전을 전달해주기 위한 dto 구현

--- co-write/core/reading-document/src/main/java/backend/cowrite/service/eventhandler/EventHandler.java
+++ co-write/core/reading-document/src/main/java/backend/cowrite/service/eventhandler/EventHandler.java
@@ -3,8 +3,9 @@ package backend.cowrite.service.eventhandler;
 
 import backend.cowrite.common.event.Event;
 import backend.cowrite.common.event.EventPayload;
+import backend.cowrite.service.dto.EditedResult;
 
 public interface EventHandler<T extends EventPayload> {
-    void handle(Long documentId, Event<T> event);
+    EditedResult handle(Long documentId, Event<T> event);
     boolean supports(Event<T> event);
 }
