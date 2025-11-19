5a0977a9abc39d138f65f81ced02d796835b4eab
jeongseonwoong <tjsdnd1219@naver.com>
2025-11-19 11:29:24 +0900

    feat(EventHandler): 이벤트핸들러 인터페이스 정의

--- co-write/core/user-and-document/src/main/java/backend/cowrite/service/eventhandler/EventHandler.java
+++ co-write/core/user-and-document/src/main/java/backend/cowrite/service/eventhandler/EventHandler.java
@@ -3,9 +3,9 @@ package backend.cowrite.service.eventhandler;
 
 import backend.cowrite.common.event.Event;
 import backend.cowrite.common.event.EventPayload;
-import backend.cowrite.service.dto.EditedResult;
 
 public interface EventHandler<T extends EventPayload> {
-    EditedResult handle(Long documentId, Event<T> event);
+    void handle(Long documentId, Event<T> event);
+
     boolean supports(Event<T> event);
 }
