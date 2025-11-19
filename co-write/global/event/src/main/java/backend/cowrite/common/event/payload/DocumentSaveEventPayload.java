d011494f5001735b566dab30b678921c13a90c7a
jeongseonwoong <tjsdnd1219@naver.com>
2025-11-19 10:47:25 +0900

    feat(EventPayload): 문서 저장을 위한 이벤트페이로드 구현

--- co-write/global/event/src/main/java/backend/cowrite/common/event/payload/DocumentSaveEventPayload.java
+++ co-write/global/event/src/main/java/backend/cowrite/common/event/payload/DocumentSaveEventPayload.java
@@ -1,2 +1,15 @@
-package backend.cowrite.common.event.payload;public class DocumentSaveEventPayload {
+package backend.cowrite.common.event.payload;
+
+import backend.cowrite.common.event.EventPayload;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocumentSaveEventPayload implements EventPayload {
+    private String editedContent;
 }
