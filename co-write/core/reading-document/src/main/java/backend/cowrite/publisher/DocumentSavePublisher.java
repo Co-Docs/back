34f5699ed7fb316d9b590b982250f64acf0fd52e
jeongseonwoong <tjsdnd1219@naver.com>
2025-11-19 16:21:25 +0900

    fix(EventPublisher): EventType이 Update로 되어있던 부분 SAVE로 수정

--- co-write/core/reading-document/src/main/java/backend/cowrite/publisher/DocumentSavePublisher.java
+++ co-write/core/reading-document/src/main/java/backend/cowrite/publisher/DocumentSavePublisher.java
@@ -18,6 +18,6 @@ public class DocumentSavePublisher {
         DocumentSaveEventPayload documentSaveEventPayload = DocumentSaveEventPayload.builder()
                 .editedContent(editedContent)
                 .build();
-        outboxEventPublisher.publish(EventType.UPDATE, documentSaveEventPayload, documentId);
+        outboxEventPublisher.publish(EventType.SAVE, documentSaveEventPayload, documentId);
     }
 }
