4573a6da5d6ba1230da240fb44d77fc5431cfcf6
jeongseonwoong <tjsdnd1219@naver.com>
2025-11-19 10:54:55 +0900

    feat(publisher): 문서 저장 이벤트 퍼블리셔 구현

--- co-write/core/reading-document/src/main/java/backend/cowrite/publisher/DocumentSavePublisher.java
+++ co-write/core/reading-document/src/main/java/backend/cowrite/publisher/DocumentSavePublisher.java
@@ -1,2 +1,23 @@
-package backend.cowrite.publisher;public class DocumentSavePublisher {
+package backend.cowrite.publisher;
+
+import backend.cowrite.common.event.EventType;
+import backend.cowrite.common.event.payload.DocumentSaveEventPayload;
+import backend.cowrite.common.outboxmessagerelay.pub.OutboxEventPublisher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class DocumentSavePublisher {
+
+    private final OutboxEventPublisher outboxEventPublisher;
+
+    @Transactional
+    public void saveDocument(Long documentId, String editedContent) {
+        DocumentSaveEventPayload documentSaveEventPayload = DocumentSaveEventPayload.builder()
+                .editedContent(editedContent)
+                .build();
+        outboxEventPublisher.publish(EventType.UPDATE, documentSaveEventPayload, documentId);
+    }
 }
