0916c5ba66336ea25ccf68aed17bb23ef9541bc7
jeongseonwoong <tjsdnd1219@naver.com>
2025-11-23 13:41:26 +0900

    refactor: 모듈 이름 reading-document -> edit-document로 변경

--- co-write/core/edit-document/src/main/java/backend/cowrite/ReadingDocumentApplication.java
+++ co-write/core/edit-document/src/main/java/backend/cowrite/ReadingDocumentApplication.java
@@ -4,6 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReadingDocumentApplication {
-    public static void main(String[] args) {SpringApplication.run(ReadingDocumentApplication.class, args);}
+public class EditDocumentApplication {
+    public static void main(String[] args) {SpringApplication.run(EditDocumentApplication.class, args);}
 }
