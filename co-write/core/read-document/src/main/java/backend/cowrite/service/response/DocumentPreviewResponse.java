0a92b14199af46b3102fd3cdce3e095ac2caa6b6
jeongseonwoong <tjsdnd1219@naver.com>
2026-03-15 15:31:44 +0900

    feat : 문서 미리보기용 dto 구현

--- co-write/core/read-document/src/main/java/backend/cowrite/service/response/DocumentPreviewResponse.java
+++ co-write/core/read-document/src/main/java/backend/cowrite/service/response/DocumentPreviewResponse.java
@@ -1,2 +1,15 @@
-package backend.cowrite.service.response;public class DocumentPreviewResponse {
+package backend.cowrite.service.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public record DocumentPreviewResponse(
+        String title,
+        List<String> userNicknames
+) {
+
 }
