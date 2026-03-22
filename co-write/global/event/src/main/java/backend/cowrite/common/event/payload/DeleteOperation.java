11f414387f9262fbc8655a8b6b5cfc2cc7d9ec18
jeongseonwoong <tjsdnd1219@naver.com>
2026-03-23 06:04:08 +0900

    refactor: operationId Long -> String 변경

--- co-write/global/event/src/main/java/backend/cowrite/common/event/payload/DeleteOperation.java
+++ co-write/global/event/src/main/java/backend/cowrite/common/event/payload/DeleteOperation.java
@@ -1,5 +1,6 @@
 package backend.cowrite.common.event.payload;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DeleteOperation implements Operation{
     int targetPosition;
     int operationCount;
